e2841e0f0c2028baa4a3e51a2f3ffc853dea4857
piavargas <piavargascarrasco@gmail.com>
2022-08-10 20:55:01 +0200

    Update Inloggad.java

--- src/MIBprojekt22/Inloggad.java
+++ src/MIBprojekt22/Inloggad.java
@@ -12,49 +12,13 @@ import oru.inf.InfDB;
  * @author piava
  */
 public class Inloggad {
-    private InfDB idb;
-    private static String valdAnvandare;
     private static String anvandare;
-    private static String losenord;
-    private boolean loggedAcc;
+    private static boolean IsITAdmin;
     
-    
-    public Inloggad (InfDB idb, String vald, String userID, String password){
-            this.idb = idb;
-            this.valdAnvandare = vald;
-            this.anvandare = userID;
-            this.losenord = password;
-            this.loggedAcc = false;
-            loginUser();
-    }
-
-    Inloggad() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    Inloggad(String anvandare, boolean b) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    Inloggad(String anvandare, boolean b) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-     
-    private void loginUser(){
-        switch (valdAnvandare){
-            case "Agent":
-                loginAgent();
-                break;
-            case "Alien":
-                loginAlien();
-                break;
-        }
+    public Inloggad (String anvandarID, boolean IsItAdmin) {
         
-        
-    
-        
-    }
-            
+   
+   
         
             
             
