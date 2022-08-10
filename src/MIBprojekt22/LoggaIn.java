77c459d3c201af709080babdba44c68cb78f3d13
piavargas <piavargascarrasco@gmail.com>
2022-08-10 20:54:59 +0200

    Update LoggaIn.java

--- src/MIBprojekt22/LoggaIn.java
+++ src/MIBprojekt22/LoggaIn.java
@@ -181,47 +181,24 @@ public class LoggaIn extends javax.swing.JFrame {
             }
 
             if (losenord.equals(agentLosen)){
-
-                //if (Validering.agentISAdmin)
+                
+                if (Validering.IsItAdmin(anvandare)){
+                    new Inloggad (anvandare, true);
+                    setVisible(false);
+                    new AgentSida(idb).setVisible(true);
+                }
+                else {
+                    new Inloggad (anvandare, false);
+                    setVisible(false);
+                    new AgentSida(idb).setVisible(true);
+                }
+            }else {
+                LBfelaktigt.setText("Fel lösenord eller ID!");
             }
         }
     }//GEN-LAST:event_BTNloginActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(LoggaIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(LoggaIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(LoggaIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(LoggaIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new LoggaIn().setVisible(true);
-            }
-        });
-    }
+   
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BTNlogin;
