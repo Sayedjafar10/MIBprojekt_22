1e8625c2b7a3d110e11d9421e7742ebe63c8b3da
piavargas <piavargascarrasco@gmail.com>
2022-08-18 15:12:20 +0200

    Update Inloggad.java

--- src/MIBprojekt22/Inloggad.java
+++ src/MIBprojekt22/Inloggad.java
@@ -12,21 +12,18 @@ import oru.inf.InfDB;
  * @author piava
  */
 public class Inloggad {
-    private static String anvandare;
-    private static boolean IsITAdmin;
-    
+    private static String anvandareID;
+    private static boolean IsItAdmin;
+
     public Inloggad (String anvandarID, boolean IsItAdmin) {
         
+        this.anvandareID = anvandarID;
+        this.IsItAdmin = IsItAdmin;
+    }
    
-   
+    public static String getInloggadID() {
+       return anvandareID;
+    }
         
-            
-            
-    
-    
-    
-          
-    
-    
 }
-}
+
