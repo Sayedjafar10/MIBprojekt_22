e982abe0bc42817bce828f0397bfa07728d95c5c
piavargas <piavargascarrasco@gmail.com>
2022-08-24 21:11:19 +0200

    Update VisaAgentInformation.java

--- src/MIBprojekt22/VisaAgentInformation.java
+++ src/MIBprojekt22/VisaAgentInformation.java
@@ -44,6 +44,7 @@ public class VisaAgentInformation extends javax.swing.JFrame {
         TXTregdatum = new javax.swing.JTextField();
         jLabel8 = new javax.swing.JLabel();
         CBvaljagent = new javax.swing.JComboBox<>();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -79,6 +80,13 @@ public class VisaAgentInformation extends javax.swing.JFrame {
 
         CBvaljagent.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Välj agent", "Agent O", "Agent Z", "Agent J", "Agent K" }));
 
+        jButton1.setText("Visa");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -111,6 +119,10 @@ public class VisaAgentInformation extends javax.swing.JFrame {
                         .addGap(14, 14, 14)
                         .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(40, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(14, 14, 14))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -145,7 +157,9 @@ public class VisaAgentInformation extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(TXTregdatum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel7))
-                .addContainerGap(56, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addContainerGap(16, Short.MAX_VALUE))
         );
 
         pack();
@@ -154,6 +168,10 @@ public class VisaAgentInformation extends javax.swing.JFrame {
     private void TXTagentIDActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TXTagentIDActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_TXTagentIDActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -198,6 +216,7 @@ public class VisaAgentInformation extends javax.swing.JFrame {
     private javax.swing.JTextField TXTregdatum;
     private javax.swing.JTextField TXTtelnmr;
     private javax.swing.JTextField TXtadmin;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
