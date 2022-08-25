3fb08b5a8fc1637cd6930b845dfbf657f59dcb67
piavargas <piavargascarrasco@gmail.com>
2022-08-26 00:17:45 +0200

    Update VisaAgentInformation.java

--- src/MIBprojekt22/VisaAgentInformation.java
+++ src/MIBprojekt22/VisaAgentInformation.java
@@ -4,11 +4,16 @@
  */
 package MIBprojekt22;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import oru.inf.InfDB;
+import java.util.HashMap;
+import oru.inf.InfException;
+
 
 /**
  *
- * @author piava
+ * @author Pia Vargas, Amanda Demir
  */
 public class VisaAgentInformation extends javax.swing.JFrame {
 
@@ -17,6 +22,7 @@ public class VisaAgentInformation extends javax.swing.JFrame {
     public VisaAgentInformation(InfDB idb) {
         initComponents();
         this.idb = idb;
+        hamtaAgenter();
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
     }
 
@@ -34,17 +40,16 @@ public class VisaAgentInformation extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
-        TXTagentID = new javax.swing.JTextField();
-        TXTnamn = new javax.swing.JTextField();
-        TXTtelnmr = new javax.swing.JTextField();
-        TXTomradeID = new javax.swing.JTextField();
-        TXtadmin = new javax.swing.JTextField();
-        TXTregdatum = new javax.swing.JTextField();
         jLabel8 = new javax.swing.JLabel();
         CBvaljagent = new javax.swing.JComboBox<>();
-        jButton1 = new javax.swing.JButton();
+        BTNvisa = new javax.swing.JButton();
+        LBagentID = new javax.swing.JLabel();
+        LBnamn = new javax.swing.JLabel();
+        LBtelnmr = new javax.swing.JLabel();
+        LBomradeID = new javax.swing.JLabel();
+        LBadmin = new javax.swing.JLabel();
+        LBregdatum = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -63,29 +68,22 @@ public class VisaAgentInformation extends javax.swing.JFrame {
         jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         jLabel5.setText("OmrådeID:");
 
-        jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        jLabel6.setText("Administratör:");
-
         jLabel7.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         jLabel7.setText("Registreringsdatum:");
-
-        TXTagentID.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                TXTagentIDActionPerformed(evt);
-            }
-        });
 
         jLabel8.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         jLabel8.setText("Välj Agent:");
 
-        CBvaljagent.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Välj agent", "Agent O", "Agent Z", "Agent J", "Agent K" }));
-
-        jButton1.setText("Visa");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        BTNvisa.setText("Visa");
+        BTNvisa.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                BTNvisaActionPerformed(evt);
             }
         });
+
+        LBagentID.setText(" ");
+
+        LBnamn.setText("   ");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -98,31 +96,27 @@ public class VisaAgentInformation extends javax.swing.JFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(jLabel8)
                             .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel6)
                             .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel5)
                             .addComponent(jLabel4)
                             .addComponent(jLabel3))
                         .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                        .addComponent(TXTagentID)
-                                        .addComponent(TXTnamn)
-                                        .addComponent(TXTregdatum, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                    .addComponent(TXTtelnmr, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addComponent(TXTomradeID, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(TXtadmin, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(CBvaljagent, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(CBvaljagent, 0, 110, Short.MAX_VALUE)
+                            .addComponent(LBagentID, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(LBnamn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(LBtelnmr, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(LBomradeID, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(LBadmin, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(LBregdatum, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(14, 14, 14)
                         .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(40, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(14, 14, 14))
+                .addComponent(BTNvisa)
+                .addGap(18, 18, 18))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -135,44 +129,80 @@ public class VisaAgentInformation extends javax.swing.JFrame {
                     .addComponent(jLabel8))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(TXTagentID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2))
-                .addGap(12, 12, 12)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(TXTnamn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3))
+                    .addComponent(jLabel2)
+                    .addComponent(LBagentID))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(TXTtelnmr, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel4))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(TXTomradeID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel5))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(TXtadmin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel6))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(TXTregdatum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel7))
+                    .addComponent(jLabel3)
+                    .addComponent(LBnamn))
                 .addGap(18, 18, 18)
-                .addComponent(jButton1)
-                .addContainerGap(16, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(LBtelnmr)
+                    .addComponent(jLabel4))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(LBomradeID)
+                .addGap(8, 8, 8)
+                .addComponent(jLabel5)
+                .addGap(10, 10, 10)
+                .addComponent(jLabel7)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(LBadmin)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
+                .addComponent(LBregdatum)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(BTNvisa)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void TXTagentIDActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TXTagentIDActionPerformed
+    private void BTNvisaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BTNvisaActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_TXTagentIDActionPerformed
+        setInfo();
+    }//GEN-LAST:event_BTNvisaActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
-
+    private void hamtaAgenter(){
+        try {
+            ArrayList<String> agenter = new ArrayList<>();
+            agenter = idb.fetchColumn("SELECT Namn FROM Agent");
+            for (String namn : agenter){
+                CBvaljagent.addItem(namn);
+               
+            }
+        } catch (InfException ettUndantag){
+            JOptionPane.showMessageDialog(null, "Något har gått fel!");
+        }
+    }
+    
+    public void setInfo(){
+        HashMap<String, String> info;
+        info = getInfoAgent();
+                
+           String namn = info.get("Namn");
+           String telnmr = info.get("Telefon");
+           String agentID = info.get("Agent_ID");
+           String omrade = info.get("Omrade");
+           String ansDatum = info.get("Anstallningsdatum");
+        
+        LBnamn.setText(namn);
+        LBtelnmr.setText(telnmr);
+        LBagentID.setText(agentID);
+        LBomradeID.setText(omrade);
+        LBregdatum.setText(ansDatum);
+    }
+    
+    public HashMap getInfoAgent(){
+        HashMap<String,String> info = new HashMap<>();
+        try {
+            info = idb.fetchRow("SELECT Agent_ID, Agent.Namn, Telefon, Anstallningsdatum, Omrade FROM Agent WHERE Agent.Namn = '" + CBvaljagent.getSelectedItem().toString() + "';");
+        } catch (InfException ettUndantag){
+            JOptionPane.showMessageDialog(null, "Något har gått fel!"); 
+        }
+        return info;
+    }
+    
+    
     /**
      * @param args the command line arguments
      */
@@ -209,20 +239,19 @@ public class VisaAgentInformation extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BTNvisa;
     private javax.swing.JComboBox<String> CBvaljagent;
-    private javax.swing.JTextField TXTagentID;
-    private javax.swing.JTextField TXTnamn;
-    private javax.swing.JTextField TXTomradeID;
-    private javax.swing.JTextField TXTregdatum;
-    private javax.swing.JTextField TXTtelnmr;
-    private javax.swing.JTextField TXtadmin;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel LBadmin;
+    private javax.swing.JLabel LBagentID;
+    private javax.swing.JLabel LBnamn;
+    private javax.swing.JLabel LBomradeID;
+    private javax.swing.JLabel LBregdatum;
+    private javax.swing.JLabel LBtelnmr;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     // End of variables declaration//GEN-END:variables
